cf02ddbbf87d8f6d47897c2746b5fb670eff58c1
Pat Seade <pattimint6@gmail.com>
2018-12-19 15:33:49 -0600

    added to components

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
 import React, { Component } from "react";
+// import FriendCard from "./components/FriendCard";
+// import Wrapper from "./components/Wrapper";
+// import Titile from "./components/Title";
+// import Nav from "./components/Nav";
+import "./App.css";
+import friends from "./friends.json";
+
+class App extends Component {
+    state = {
+       friends 
+    };
+}
+
+
+export default App;
