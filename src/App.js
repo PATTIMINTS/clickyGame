313bef0209e7bba3c78f37c4f2f1d40e606a3ac8
Pat Seade <pattimint6@gmail.com>
2018-12-19 18:15:43 -0600

    css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
-// import FriendCard from "./components/FriendCard";
-// import Wrapper from "./components/Wrapper";
-// import Titile from "./components/Title";
-// import Nav from "./components/Nav";
-import "./App.css";
+import FriendCard from "./components/FriendCard";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 import friends from "./friends.json";
+import "./App.css";
 
 class App extends Component {
+    // Setting this.state.friends to the friends json array
     state = {
-       friends 
+        friends
     };
-}
 
+    //   removeFriend = id => {
+    //     // Filter this.state.friends for friends with an id not equal to the id being removed
+    //     const friends = this.state.friends.filter(friend => friend.id !== id);
+    //     // Set this.state.friends equal to the new friends array
+    //     this.setState({ friends });
+    //   };
+
+    // Map over this.state.friends and render a FriendCard component for each friend object
+    render() {
+        return (
+            <Wrapper>
+                <Nav
+                    title="Sunny in Phili Friends click Game"
+                    score={this.state.currentScore}
+                    topScore={this.state.topScore}
+                    rORw={this.state.rORw}>
+                </Nav>
+                <Title>Friends List</Title>
+                {this.state.friends.map(friend => (
+                    <FriendCard
+                        
+                        id={friend.id}
+                        key={friend.id}
+                        name={friend.name}
+                        image={friend.image}
+                    
+                    />
+                ))}
+            </Wrapper>
+        );
+    }
+}
 
 export default App;
