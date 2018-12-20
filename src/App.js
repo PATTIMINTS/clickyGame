071e3c67ad85ffe85a34c52a9418391f1333fbd4
Pat Seade <pattimint6@gmail.com>
2018-12-20 00:49:48 -0600

    app.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,64 @@ import Title from "./components/Title";
 import friends from "./friends.json";
 import "./App.css";
 
+function shuffleFriends(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+
 class App extends Component {
     // Setting this.state.friends to the friends json array
     state = {
-        friends
+        friends,
+        currentScore: 0,
+        topScore: 0,
+        eORw: "",
+        clicked: [],
+
+
     };
 
-    //   removeFriend = id => {
-    //     // Filter this.state.friends for friends with an id not equal to the id being removed
-    //     const friends = this.state.friends.filter(friend => friend.id !== id);
-    //     // Set this.state.friends equal to the new friends array
-    //     this.setState({ friends });
-    //   };
+    handleClick = id => {
+        if (this.state.clicked.indexOf(id) === -1) {
+            this.handleIncrement();
+            this.setState({ clicked: this.state.clicked.concat(id) });
+        } else {
+            this.handleReset();
+        }
+    };
+    handleIncrement = () => {
+        const newScore = this.state.currentScore + 1;
+        this.setState({
+            currentScore: newScore,
+            rORw: ""
+        });
+        if (newScore >= this.state.topScore) {
+            this.setState({ topScore: newScore });
+        }
+        else if (newScore === 12) {
+            this.setState({ rORw: "You win!" });
+        }
 
-    // Map over this.state.friends and render a FriendCard component for each friend object
+        this.handleShuffle();
+    }; handleReset = () => {
+        this.setState({
+            currentScore: 0,
+            topScore: this.state.topScore,
+            rORw: "Glaven!",
+            clicked: []
+        });
+        this.handleShuffle();
+    };
+    handleShuffle = () => {
+        let shuffledFriends = shuffleFriends(friends);
+        this.setState({ friends: shuffledFriends });
+    };
+
+
     render() {
         return (
             <Wrapper>
@@ -32,12 +76,16 @@ class App extends Component {
                 <Title>Friends List</Title>
                 {this.state.friends.map(friend => (
                     <FriendCard
-                        
+
                         id={friend.id}
                         key={friend.id}
+                        handleClick={this.handleClick}
+                        handleIncrement={this.handleIncrement}
+                        handleReset={this.handleReset}
+                        handleShuffle={this.handleShuffle}
                         name={friend.name}
                         image={friend.image}
-                    
+
                     />
                 ))}
             </Wrapper>
--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -3,7 +3,9 @@ import "./FriendCard.css";
 
 const FriendCard = props => (
     
-  <div className="card">
+  <div className="card"
+  value={props.id} 
+  onClick={() => props.handleClick(props.id)}>
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
