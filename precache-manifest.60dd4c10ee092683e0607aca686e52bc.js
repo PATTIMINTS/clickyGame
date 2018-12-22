d4d5c7ed0bd65b04807fb96e9ee1b3b616549fea
Pat Seade <pattimint6@gmail.com>
2018-12-22 10:49:21 -0600

    Updates

---
+++ precache-manifest.60dd4c10ee092683e0607aca686e52bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "203df38940cc4613e145",
+    "url": "/clickygame/static/css/main.6e98278d.chunk.css"
+  },
+  {
+    "revision": "203df38940cc4613e145",
+    "url": "/clickygame/static/js/main.203df389.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/clickygame/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "20131616c4f095a2b73d893811d78a39",
+    "url": "/clickygame/index.html"
+  }
+];
